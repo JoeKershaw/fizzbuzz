5577cb4df0bc825abba33f4e8cca78e382e34aca
Joseph <jpkershaw17@gmail.com>
2020-10-08 09:02:52 +0100

    Joe after chat with eddie

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
+  if number % 3 != 0 && number % 5 != 0 then return number end
   if number % 3 == 0 && number % 5 == 0 then return 'fizzbuzz' end
   if number % 3 == 0 then return 'fizz' end
   if number % 5 == 0 then return 'buzz' end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,23 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
+  x = (1..300)
+
   it 'returns "fizz" when passed multiple of 3' do
-    x = [3,6,9,12,18,21,24,27]
+    x = x.select {|num| num % 3 == 0 && num % 5 != 0}
     x.each do |x| expect(fizzbuzz(x)).to eq 'fizz' end
   end
   it 'return buzz when passed multiple of 5' do
-    x = [5,10,20,25,35,40]
-    x.each do |x|expect(fizzbuzz(x)).to eq 'buzz' end
+    x = x.select {|num| num % 5 == 0 && num % 3 != 0}
+    x.each do |x| expect(fizzbuzz(x)).to eq 'buzz' end
   end
   it "return'fizzbuzz' when passed common multiple of 3 and 5 "do
-    x = [15,30,45,60,75,90]
-    x.each do |x|expect(fizzbuzz(x)).to eq 'fizzbuzz' end
+    x = x.select {|num| num % 3 == 0 && num % 5 == 0}
+    x.each do |x| expect(fizzbuzz(x)).to eq 'fizzbuzz' end
+  end
+  it "returns the number for any non-multiple of 3 or 5" do
+    x = x.select {|num| num % 3 != 0 && num % 5 != 0}
+    x.each do |x| expect(fizzbuzz(x)).to eq x end
   end
 end
